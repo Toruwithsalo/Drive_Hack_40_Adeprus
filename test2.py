9b442d034268e72eb123ee41a8a236789fe43cec
Toruwithsalo <100860315+Toruwithsalo@users.noreply.github.com>
2025-10-15 20:55:13 +0300

    I fixed it

--- test2.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = "https://gigachat.devices.sberbank.ru/api/v1/models"
-payload={}
-headers = {
-  'Accept': 'application/json',
-  'Authorization': "Classsic MDE5OWU2ZWQtNzZhYy03NDRiLWI1MzAtMGMzYmMxNDdhZGM0OjNlZDZkMjM3LWM0NDQtNGRjYS1hM2JjLTAwMTA3Y2U1MzQzNg=="
-}
-response = requests.get( url, headers=headers, data=payload, verify=False)
-print(response)
