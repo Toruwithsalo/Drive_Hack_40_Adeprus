702a2cad883b9225b92195039a6b54e765d67d04
Toruwithsalo <100860315+Toruwithsalo@users.noreply.github.com>
2025-10-15 13:57:05 +0300

    Duh

--- gigaapi.py
+++ gigaapi.py
@@ -9,9 +9,9 @@ headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
   'Accept': 'application/json',
   'RqUID': '19adb04b-609b-414c-82e6-8848942ccfc2',
-  'Authorization': 'Basic <Authorization key>'
+  'Authorization': 'MDE5OWU2ZWQtNzZhYy03NDRiLWI1MzAtMGMzYmMxNDdhZGM0OmIxMTAxYTZlLTBkYmQtNDNlZS1hMzU3LWJhZjQwZTZhNWVhYg=='
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response = requests.request("POST", url, headers=headers, data=payload, verify = False)
 
 print(response.text)
